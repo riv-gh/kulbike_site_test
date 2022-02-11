81601aef4b032d82f6979e823809986187177155
daenur <daenur@outlook.com>
2022-02-11 12:46:56 +0200

    test1

---
+++ src/js/google_company.js
@@ -0,0 +1,9 @@
+//https://maps.googleapis.com/maps/api/place/details/output?parameters
+const PLACE_ID = 'ChIJe_DHaSnF1EARa62oP8tFwLQ';
+const API_KEY = '';
+
+
+`https://maps.googleapis.com/maps/api/place/details/json
+ ?fields=name%2Crating%2Cformatted_phone_number
+ &place_id=${PLACE_ID}
+ &key=${API_KEY}`
