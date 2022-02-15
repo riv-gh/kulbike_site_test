888fabc4d2b77f63ddce0493f82f1aff65c1bd41
daenur <daenur@outlook.com>
2022-02-15 11:40:28 +0200

    mobile menu animation

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,3 +1,7 @@
+const menu_el = document.getElementById('menu')
 function toggleMenu() {
-    document.getElementById('menu').classList.toggle('show');
+    menu_el.classList.toggle('show')
 }
+document.querySelectorAll('#menu a:not(#menu_btn)').forEach(a=>{
+    a.addEventListener('click', () => { menu_el.classList.remove('show') })
+})
