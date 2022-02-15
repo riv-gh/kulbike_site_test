2f3f178a97ba628f45ff203d5a999193ced4da72
daenur <daenur@outlook.com>
2022-02-15 13:38:59 +0200

    Markdown link

--- src/js/send_message.js
+++ src/js/send_message.js
@@ -1,11 +1,11 @@
 const form_btn = document.querySelector('#message-form input[type=button]');
 form_btn.addEventListener('click', async (form) => {
     return send_message(
-        'Имя: ' + document.getElementById('tb_name').value + '\n' +
+        'Имя: *' + document.getElementById('tb_name').value + '*\n' +
         'Телефон: ' + document.getElementById('tb_phone').value + '\n' +
         ( document.getElementById('cb_write').checked ? 'Предпочтение текста\n' : 'Предпочтение звонка\n') +
-        'Сообщение:\n' + document.getElementById('tb_message').value + '\n' +
-        document.location.origin+document.location.pathname + 'recall.html?' + document.getElementById('tb_phone').value.replace('+','')
+        'Сообщение:\n' + document.getElementById('tb_message').value + '\n\n' +
+        '[‌‌Перезвонить](' + document.location.origin+document.location.pathname + 'recall.html?' + document.getElementById('tb_phone').value.replace('+','') + ')'
     )
         
 })
@@ -19,7 +19,7 @@ function send_message(text) {
     fetch(link, {
     method: 'post',
     headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: `chat_id=${telegram_chat_id}&text=${encodeURI(text)}`,
+    body: `chat_id=${telegram_chat_id}&parse_mode=Markdown&text=${encodeURI(text)}`,
     }).then((response) => {
         return response.text()
     }).then((text) => {
