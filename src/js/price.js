00fb1077a7695104750984f7ae97bcc35330a2c9
daenur <daenur@outlook.com>
2022-02-15 08:10:42 +0200

    price

--- src/js/price.js
+++ src/js/price.js
@@ -3,11 +3,12 @@ fetch('./price.txt').then((response) => {
 }).then((text_price) => {
 	document.getElementById('price-table').innerHTML = 
     text_price.split('\n').map(line => {
-        return '<tr>' + line.split('\t').map(cell => {
-            return '<td>' + cell + '</td>';
-        }).join('') + '</tr>';
-    }).join('\n');
+        return '<tr>' + line.split('\t').map((cell, i, arr) => {
+            return (arr.length-1) ? '<td>' + cell + '</td>' : '<td class="table-header" colspan="2">' + cell + '</td>'
+        }).join('') + '</tr>'
+    }).join('\n')
 }).catch((err) => {
 	console.warn('Ошибка загрузки текста', err)
     document.getElementById('price-table').textContent = 'Прайс-лист временно недоступен...'
-});
+})
+
